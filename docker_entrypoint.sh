2ae2af4332e992a36e0b3e8370b3bac32e303b50
kn0wmad <kn0wmad@protonmail.com>
2021-09-16 22:22:50 -0600

    entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+set -euo pipefail
 
 export HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
+export PEER_TOR_ADDRESS=$(yq e '.peer-tor-address' /root/.monero/start9/config.yaml)
+export RPC_TOR_ADDRESS=$(yq e '.rpc-tor-address' /root/.monero/start9/config.yaml)
 
-exec tini monero
+exec /usr/local/bin/monerod-manager
