0115ae89b44ef806175d617d72c168a160983f08
kn0wmad <39687477+kn0wmad@users.noreply.github.com>
2022-10-02 21:16:24 -0600

    0.18.1.1 (#7)
    * Manifest init
    
    * Misc updates
    
    * Makefile
    
    * Manager init
    
    * Manager edits
    
    * Progressing through Dockerfile
    
    * Slogging through build errors
    
    * Holy shit it built
    
    * Working on manifest
    
    * entrypoint
    
    * config_spec
    
    * Minor edits
    
    * Typo fix
    
    * Update to 0.17.3, fix Dockerfile
    
    * update .gitignore
    
    * update LICENSE
    
    * Delete monerod.s9pk
    
    * update manifest
    
    * Builds, not running, WIP
    
    * idkwtf, it's all bad
    
    * quote, working, endquote
    
    * sort of launches
    
    * Removed monero user, updated to v0.18
    
    * Updated to embassy scripts, monero 18.1
    
    * refactor
    
    * werks
    
    * SUPPORT THE NETWORK
    
    * Conf edit, expose 18083
    
    * Update to 0.18.1.1, add instructions, fix healthcheck
    
    Co-authored-by: Dread <dread@start9labs.com>

--- scripts/check-rpc.sh
+++ scripts/check-rpc.sh
@@ -4,13 +4,10 @@ DURATION=$(</dev/stdin)
 if (($DURATION <= 5500)); then
     exit 60
 else
-    curl -X POST http://monerod.embassy:18089/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"get_version"}' -H 'Content-Type: application/json' &>/dev/null
+    curl http://localhost:18081/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"get_info"}' -H 'Content-Type: application/json' &>/dev/null
     RES=$?
     if [ $RES != 0 ]; then
         echo "Monero RPC is unreachable" >&2
         exit 1
     fi
 fi
-
-
-
