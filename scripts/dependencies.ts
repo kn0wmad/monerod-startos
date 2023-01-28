06beccb0eebca1ba90e52e7025c23691a93f5d34
kn0wmad <kn0wmad@protonmail.com>
2023-01-27 20:44:32 -0700

    Update sdk dep for scripts

--- scripts/dependencies.ts
+++ scripts/dependencies.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.8-alpha2/mod.ts";
-export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.8-alpha2/healthUtil.ts";
+export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.8/mod.ts";
+export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.8/healthUtil.ts";
