d0c75b3fceb87faac80f4e69ae17ffa1c7211a26
kn0wmad <kn0wmad@protonmail.com>
2023-04-12 13:34:37 -0600

    Fix health check

--- scripts/check-sync.sh
+++ scripts/check-sync.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
 
-DURATION=$(</dev/stdin)
-if (($DURATION <= 5500)); then
-    exit 60
-else
-    #STATUS=$(monerod --non-interactive --config-file /data/.bitmonero/monero.conf status | grep -o 'Height.*mainnet')
-    STATUS=$(curl -s http://127.0.0.1:18081/get_info -H 'Content-Type: application/json')
-    STATUS_EXIT_CODE=$?
-    #SYNC_STATUS=$(echo $STATUS | sed "s/^Height: \([0-9]*\)\/\([0-9]*\) (\([0-9]*\)\.\([0-9]\)%.*/\1:\2:\3/")
-    #SYNC_PROGRESS=$(echo $SYNC_STATUS | cut -d: -f3 | cut -d'.' -f1)
-    SYNCED=$(echo $STATUS | yq e '.synchronized')
-    #BLOCKS_TOTAL=$(echo $SYNC_STATUS | cut -d: -f2)
-    BLOCKS_SYNCED=$(echo $STATUS | yq e '.height')
-    BLOCKS_TOTAL=$(echo $STATUS | yq e '.target_height')
-    if [[ $STATUS_EXIT_CODE -ne 0 ]]; then
-     echo "Monero RPC is unreachable"
-     exit 61
-    elif [ "$SYNCED" = "true" ] ; then
-     #Blockchain is synced, yay
-     echo "Blockchain is synced"
-     exit 0
-    elif [ "$SYNCED" = "false" ] ; then
-     echo -n "Syncing Monero blockchain.  Initial sync may take several days.  STATUS: Processed block #$BLOCKS_SYNCED"
-     if [[ $BLOCKS_TOTAL -gt 0 ]] ; then
-      SYNC_PROGRESS=$(expr ${BLOCKS_SYNCED}00 / $BLOCKS_TOTAL)
-      echo -n "/${BLOCKS_TOTAL} ($SYNC_PROGRESS%)"
-     fi
-     #echo ""
-     exit 61
-    else
-     echo "Error ascertaining blockchain status"
-     exit 61
+STATUS=$(curl -s http://127.0.0.1:18081/get_info -H 'Content-Type: application/json')
+STATUS_EXIT_CODE=$?
+SYNCED=$(echo $STATUS | yq e '.synchronized')
+BLOCKS_SYNCED=$(echo $STATUS | yq e '.height')
+BLOCKS_TOTAL=$(echo $STATUS | yq e '.target_height')
+
+if [[ $STATUS_EXIT_CODE -ne 0 ]]; then
+    echo "Monero RPC is unreachable"
+    exit 61
+elif [ "$SYNCED" = "true" ] ; then
+    echo "Blockchain is synced"
+    exit 0
+elif [ "$SYNCED" = "false" ] ; then
+    echo -en "Syncing Monero blockchain.  Initial sync may take several days. STATUS: Syncing block #$BLOCKS_SYNCED " >&2
+    if [[ $BLOCKS_TOTAL -gt 0 ]] ; then
+    SYNC_PROGRESS=$(expr ${BLOCKS_SYNCED}00 / $BLOCKS_TOTAL)
+    echo -n "/ ${BLOCKS_TOTAL} ($SYNC_PROGRESS%)" >&2
     fi
+    exit 61
+else
+    echo "Error ascertaining blockchain status"
+    exit 61
 fi
