2261003265ec222ec210709fc28e19e7889503c0
gStart9 <george@start9labs.com>
2023-04-25 22:53:14 -0600

    spacing & eliminate p2p interface from manifest

--- scripts/check-sync.sh
+++ scripts/check-sync.sh
@@ -13,13 +13,13 @@ elif [ "$SYNCED" = "true" ] ; then
     echo "Blockchain is synced"
     exit 0
 elif [ "$SYNCED" = "false" ] ; then
-    echo -en "Syncing Monero blockchain.  Initial sync may take several days. STATUS: Syncing block #$BLOCKS_SYNCED" >&2
+    echo -n "Syncing Monero blockchain.  Initial sync may take several days. STATUS: Syncing block #$BLOCKS_SYNCED" >&2
     if [[ $BLOCKS_TOTAL -gt 0 ]] ; then
-    SYNC_PROGRESS=$(expr ${BLOCKS_SYNCED}00 / $BLOCKS_TOTAL)
-    echo -n " / ${BLOCKS_TOTAL} ($SYNC_PROGRESS%)" >&2
+        SYNC_PROGRESS=$(expr ${BLOCKS_SYNCED}00 / $BLOCKS_TOTAL)
+        echo -n " / $BLOCKS_TOTAL ($SYNC_PROGRESS%)" >&2
     fi
     exit 61
 else
-    echo "Error ascertaining blockchain status"
-    exit 61
+     echo "Error ascertaining blockchain status"
+     exit 61
 fi
