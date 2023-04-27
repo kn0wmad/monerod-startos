c789bb3b8fe144d39e761e8c0e2cd1c1282c3b53
gStart9 <106188942+gStart9@users.noreply.github.com>
2023-04-27 19:12:56 +0000

    Fix 'Advertise RPC Node' option (#17)

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -112,8 +112,8 @@ sed -i "s/TOR_HOSTNAME/$TOR_HOSTNAME/g" $new_conf_template
 sed -i "s/TOR_PORT/$TOR_PORT/g" $new_conf_template
 sed -i "s/ADV_TOR_MAXSOCKSCONNS/$ADV_TOR_MAXSOCKSCONNS/g" $new_conf_template
 sed -i "s/RPC_TOR_ADDRESS/$RPC_TOR_ADDRESS/g" $new_conf_template
-sed -i "s/MONERO_RPC_PORT/$MONERO_RPC_PORT/g" $new_conf_template
 sed -i "s/MONERO_RPC_PORT_HS/$MONERO_RPC_PORT_HS/g" $new_conf_template
+sed -i "s/MONERO_RPC_PORT/$MONERO_RPC_PORT/g" $new_conf_template
 sed -i "s/MONERO_ANON_INBOUND_HOST/$MONERO_ANON_INBOUND_HOST/g" $new_conf_template
 sed -i "s/ADV_TOR_MAXONIONCONNS/$ADV_TOR_MAXONIONCONNS/g" $new_conf_template
 
