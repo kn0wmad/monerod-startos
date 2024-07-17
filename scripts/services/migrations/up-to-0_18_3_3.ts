69d4ca3c8f6dad0924f37de082511044cc87123b
gStart9 <george@start9labs.com>
2024-07-17 15:24:48 -0600

    0.18.3.3 version reference touchup

--- scripts/services/migrations/up-to-0_18_3_3.ts
+++ scripts/services/migrations/up-to-0_18_3_3.ts
@@ -1,7 +1,7 @@
 import { types as T, matches } from "../../deps.ts";
 const { dictionary, boolean, shape, string, any } = matches;
 
-export const migration_up_to_0_18_3_2 = (config: T.Config): T.Config => {
+export const migration_up_to_0_18_3_3 = (config: T.Config): T.Config => {
   if (Object.keys(config).length === 0) {
     // service was never configured
     return config;
