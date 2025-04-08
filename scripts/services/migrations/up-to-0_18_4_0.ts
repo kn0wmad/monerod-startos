9c299d2584d1b4a82f36d4fadcc6b975c1424ece
gStart9 <106188942+gStart9@users.noreply.github.com>
2025-04-08 17:09:33 +0000

    Update scripts/services/migrations/up-to-0_18_4_0.ts
    Co-authored-by: Lucy <12953208+elvece@users.noreply.github.com>

--- scripts/services/migrations/up-to-0_18_4_0.ts
+++ scripts/services/migrations/up-to-0_18_4_0.ts
@@ -11,9 +11,7 @@ export const migration_up_to_0_18_4_0 = (config: T.Config): T.Config => {
     !matches
       .shape({
         advanced: matches.shape({
-          p2p: matches.shape({ spynodebanlist: matches.boolean }, [
-            "spynodebanlist",
-          ]),
+          p2p: matches.shape({ spynodebanlist: matches.boolean }),
         }),
       })
       .test(config)
